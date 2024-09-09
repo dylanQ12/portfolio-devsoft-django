b9e10358468c5c2ad30e8aa9d5248c365a4e1910
dylanQ12 <136197614+dylanQ12@users.noreply.github.com>
2024-09-09 17:37:16 -0500

    agregate model SkillAdmin in skill/admin.py

--- skills/admin.py
+++ skills/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Skill
 
-# Register your models here.
-admin.site.register(Skill)
 
+class SkillAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "image",
+        "name",
+        "experience",
+    )
+    list_filter = ("id", "name", "experience")
+    search_fields = ("id", "name", "experience")
+
+
+# Register your models here.
+admin.site.register(Skill, SkillAdmin)

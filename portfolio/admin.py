b4915e847a2e95182f8ced122ce515d8b66b68b2
dylanQ12 <136197614+dylanQ12@users.noreply.github.com>
2024-09-09 17:19:37 -0500

    agregate model ProjectAdmin in portfolio/admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Project
 
+# Model ProjectAdmin.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "image",
+        "title",
+        "date",
+    )
+    list_filter = (
+        "title",
+        "date",
+    )
+    search_fields = ("id", "title", "description")
+    date_hierarchy = "date"
 
-admin.site.register(Project)
+
+admin.site.register(Project, ProjectAdmin)

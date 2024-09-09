22bde067f3ac6057443ca661f9cb93fb9ced8304
dylanQ12 <136197614+dylanQ12@users.noreply.github.com>
2024-09-09 17:28:34 -0500

    agregate model PostAdmin in blog/admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "image",
+        "title",
+        "date",
+    )
+    list_filter = (
+        "id",
+        "title",
+        "date",
+    )
+    search_fields = ("id", "title")
+    date_hierarchy = "date"
+
+
+admin.site.register(Post, PostAdmin)
